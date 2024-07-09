0cc2bd75810b8e1e350817abfd986dea440b6282
Matteo Scanferla <m.scanferla@eggonemail.com>
2024-07-09 12:12:33 +0200

    added cors 5 all

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,22 +8,12 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $allowedOrigins = [
-            'https://esame-angular.onrender.com',
-            'https://esame-laravel.onrender.com',
-            'http://localhost:4200',
-        ];
+        $response = $next($request);
 
-        $requestOrigin = $request->headers->get('Origin');
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
 
-        if (in_array($requestOrigin, $allowedOrigins)) {
-            $response = $next($request);
-            $response->headers->set('Access-Control-Allow-Origin', $requestOrigin);
-            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
-            return $response;
-        }
-
-        return response()->json(['error' => 'Unauthorized'], 403);
+        return $response;
     }
 }
